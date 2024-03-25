32da816138ce934ae5c04690b727f93ec1394a68
young <purple11_11@naver.com>
2024-03-25 17:01:15 +0900

    fix: NCP 서버 IP 수정

--- config/config.js
+++ config/config.js
@@ -12,7 +12,7 @@ const prod = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: "sesac",
-    host: "115.85.183.123", // NCP 서버 공인 IP
+    host: "118.67.134.103", // NCP 서버 공인 IP
     dialect: "mysql",
 };
 
